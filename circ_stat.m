edd31692c4699e6394e787feb831fb24f4f4a61a
gitbestio <slv.innoc@gmail.com>
2022-09-14 18:35:36 +0000

    add std, skewness and kurt

--- circ_stat.m
+++ circ_stat.m
@@ -33,6 +33,14 @@ function out  = circ_stat(fname,varargin)
         case {'med', 'median'}
             out = circ_median(varargin{:});     
 
+        case {'std'}
+            out = circ_std(varargin{:});     
+            
+        case {'skewness'}
+            [out,~] = circ_skewness(varargin{:});      
+            
+        case {'kurtosis'} 
+            [out,~] = kurtosis(varargin{:});      
 
         case {'circ_quartiles'}
             out = circ_quartiles(varargin{:});     
@@ -199,6 +207,30 @@ function S = circ_var(alpha, w, d, dim)
     % apply transformation to var
     S = 1 - r;
 end
+
+
+function S = circ_std(alpha, w, d, dim)
+%
+% s = circ_std(alpha, w, d, dim)
+%   
+% Computes circular standard dev for circular data (equ. 26.17/18, Zar).   
+%
+%   Input:
+%     alpha	sample of angles in radians
+%     [w		number of incidences in case of binned angle data]
+%     [d    spacing of bin centers for binned data, if supplied 
+%           correction factor is used to correct for bias in 
+%           estimation of r]
+%     [dim  compute along this dimension, default is 1]
+%
+%     If dim argument is specified, all other optional arguments can be
+%     left empty: circ_var(alpha, [], [], dim)
+%
+%   Output:
+%     S     circular std 2*(1-r)
+    S = 2 * circ_var(alpha, w, d, dim);
+end
+
 
 function r = circ_r(alpha, w, d, dim)
 %
@@ -382,3 +414,101 @@ function [q25,q50,q75] = circ_quartiles(x)
     q50 = mod(me,2.*pi);
         
 end
+
+
+function [b b0] = circ_skewness(alpha, w, dim)
+%
+% [b b0] = circ_skewness(alpha, w, dim)
+%
+% Computes two measures of angular skewness.
+%
+%
+%   Input:
+%     alpha     sample of angles
+%     [w        weightings in case of binned angle data]
+%     [dim      statistic computed along this dimension, 1]
+%
+%     If dim argument is specified, all other optional arguments can be
+%     left empty: circ_skewness(alpha, [], dim)
+%
+%   Output:
+%     b         skewness (from Pewsey)
+%     b0        alternative skewness measure (from Fisher, formula 2.29 in Berens 2009)
+%
+
+    
+    if nargin < 3
+      dim = 1;
+    end
+    
+    if nargin < 2 || isempty(w)
+      % if no specific weighting has been specified
+      % assume no binning has taken place
+        w = ones(size(alpha));
+    else
+      if size(w,2) ~= size(alpha,2) || size(w,1) ~= size(alpha,1) 
+        error('Input dimensions do not match');
+      end 
+    end
+    
+    
+    % compute neccessary values
+    R = circ_r(alpha,w,[],dim);
+    theta = circ_mean(alpha,w,dim);
+    [~, rho2 mu2] = circ_moment(alpha,w,2,true,dim);
+    
+    % compute skewness 
+    theta2 = repmat(theta, size(alpha)./size(theta));
+    b  = sum(w.*(sin(2*(circ_dist(alpha,theta2)))),dim)./sum(w,dim);
+    b0 = rho2.*sin(circ_dist(mu2,2*theta))./(1-R).^(3/2);    % (formula 2.29)
+end    
+    
+
+
+function [k k0] = circ_kurtosis(alpha, w, dim)
+%
+% [k k0] = circ_kurtosis(alpha, w, dim)
+%
+% Computes two measures of angular kurtosis.
+%
+%
+%   Input:
+%     alpha     sample of angles
+%     [w        weightings in case of binned angle data]
+%     [dim      statistic computed along this dimension, 1]
+%
+%     If dim argument is specified, all other optional arguments can be
+%     left empty: circ_kurtosis(alpha, [], dim)
+%
+%   Output:
+%     k         kurtosis (from Pewsey)
+%     k0        kurtosis (from Fisher, formula 2.30 of Berens 2009)
+    
+    if nargin < 3
+      dim = 1;
+    end
+    
+    if nargin < 2 || isempty(w)
+      % if no specific weighting has been specified
+      % assume no binning has taken place
+        w = ones(size(alpha));
+    else
+      if size(w,2) ~= size(alpha,2) || size(w,1) ~= size(alpha,1) 
+        error('Input dimensions do not match');
+      end 
+    end
+    
+    % compute mean direction
+    R = circ_r(alpha,w,[],dim);
+    theta = circ_mean(alpha,w,dim);
+    [~, rho2] = circ_moment(alpha,w,2,true,dim);
+    [~, ~, mu2] = circ_moment(alpha,w,2,false,dim);
+    
+    % compute skewness 
+    theta2 = repmat(theta, size(alpha)./size(theta));
+    k = sum(w.*(cos(2*(circ_dist(alpha,theta2)))),dim)./sum(w,dim);
+    k0 = (rho2.*cos(circ_dist(mu2,2*theta))-R.^4)./(1-R).^2;    % (formula 2.30)
+
+end    
+    
+    
